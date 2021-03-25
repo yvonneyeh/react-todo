413b892934eddc31b128a9e05b76fcddf8084b04
Yvonne Yeh <code@yvonneyeh.com>
2021-03-24 20:34:06 -0700

    Update TodoForm.js - useState

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 
 const TodoForm = ({ saveTodo }) => {
-    return (
-        <form>
-            <TextField variant="outlined" placeholder="Add to-do" margin="normal" />
-        </form>
-    );
+  const [value, setValue] = useState('');
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        saveTodo(value);
+      }}
+    >
+      <TextField
+        variant="outlined"
+        placeholder="Add todo"
+        margin="normal"
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+        value={value}
+      />
+    </form>
+  );
 };
 
-export default TodoForm
+export default TodoForm;
